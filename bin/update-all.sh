0b6faeb13ec2263b69d19fd494e977f61e7c7cea
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-02-11 12:15:14 -0500

    Restrict to fast-forwards pulls.

--- bin/update-all.sh
+++ bin/update-all.sh
@@ -39,7 +39,7 @@ find -maxdepth 3 -mindepth 1 -type d -name ".git" | grep "$FILTER" | while read 
     (get_upstream_repo | while read remote; do
 	git remote -v | grep ^"$remote" | grep "(fetch)$"
 	git fetch "$remote"
-	git pull "$remote"
+	git pull --ff-only "$remote"
     done)
     if [  "$STASHED" -gt 0 ];  then
 	git stash pop
