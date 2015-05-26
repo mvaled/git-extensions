714639d5170d5ed0756faf0125112311fe036ce4
Manuel Vázquez Acosta <mva.led@gmail.com>
2015-05-26 13:08:59 -0400

    Add a force option to push-all.

--- bin/push-all.sh
+++ bin/push-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Usage: ./push-all.sh [filter] [remote_name]
+# Usage: ./push-all.sh [-f] [filter] [remote_name]
 #
 #   Pushes all repositories that match (in their paths) the filter to the
 #   provided remote.  All local branches are tried to be pushed.  See
@@ -15,6 +15,13 @@
 #       ./push-all.sh . upstream
 #
 
+if [ "$1" = "-f" ]; then
+   FORCE="-f"
+   shift
+else
+   FORCE=""
+fi
+
 if [ \! -z "$1" ]; then
     FILTER="$1"
 else
@@ -27,7 +34,7 @@ find -maxdepth 3 -mindepth 1 -type d -name ".git" | grep "$FILTER" | while read 
     pushd . >/dev/null
     cd "$repo/.."
     echo -e '\E[37;44m'"\033[1m>>> `pwd`\033[0m"
-    git local-push-all "$REMOTE_NAME"
+    git local-push-all $FORCE "$REMOTE_NAME"
     git push --tags -f "$REMOTE_NAME"
     popd >/dev/null
 done
